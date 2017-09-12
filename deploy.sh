c4e44c54786a9e5d03a08d5473f183d3729bf608
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2017-09-12 11:37:09 +0200

    correct deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh pi 'rm -r /var/www/nature_coiffure/*'
-jekyll build && scp -r _site/* pi:/var/www/nature_coiffure 
+bundle exec jekyll build && scp -r _site/* pi:/var/www/nature_coiffure 
