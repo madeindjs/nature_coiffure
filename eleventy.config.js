0a2b6ee967e408cda65f43c211f1a579d4bf3b8d
Alexandre Rousseau <alexandre@rsseau.fr>
2025-09-16 22:13:07 +0200

    bundle CSS

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,4 +3,5 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function (eleventyConfig) {
   eleventyConfig.setLayoutsDirectory("_layouts");
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+  eleventyConfig.addPassthroughCopy("css/style.css");
 }
